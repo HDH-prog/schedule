8d3581b3f124f712f6257a884befeba4e623e993
green <eastking34@gmail.com>
2026-07-20 20:19:25 +0900

    예외 처리

---
+++ src/main/java/com/schedule/common/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.schedule.common;
+
+import com.schedule.common.exception.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 커스텀 에러
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<String> handleServiceException(ServiceException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(e.getMessage());
+    }
+
+    // Validation 실패 -> 400
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(fieldError -> fieldError.getDefaultMessage())
+                .orElse("입력 값이 올바르지 않습니다.");
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorMessage);
+    }
+}
