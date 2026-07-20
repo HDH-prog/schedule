9aa511c3834956f607fdd2066e438eb38e2fdb52
green <eastking34@gmail.com>
2026-07-20 11:56:37 +0900

    Schedule - Entity

---
+++ src/main/java/com/schedule/schedule/entity/Schedule.java
@@ -0,0 +1,38 @@
+package com.schedule.schedule.entity;
+
+import com.schedule.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    public Schedule(String username, String title, String content) {
+        this.username = username;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
