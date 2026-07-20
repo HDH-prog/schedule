fbd035ea5216c1eb3e539920c6d4682ca6da0a4d
green <eastking34@gmail.com>
2026-07-20 12:18:21 +0900

    fix - content 추가

--- src/main/java/com/schedule/schedule/dto/GetScheduleResponse.java
+++ src/main/java/com/schedule/schedule/dto/GetScheduleResponse.java
@@ -9,14 +9,16 @@ public class GetScheduleResponse {
     private final Long id;
     private final String username;
     private final String title;
+    private final String content;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
-    public GetScheduleResponse(Long id, String username, String title,
+    public GetScheduleResponse(Long id, String username, String title, String content,
                                 LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.id = id;
         this.username = username;
         this.title = title;
+        this.content = content;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
     }
